5fa7a38fb3844c3ef4155c975fbc4267c500caa1
FilipeAVNunes <filipenunes407@hotmail.com>
2020-12-10 18:39:18 +0000

    lesson 195, added redux thunk into saga, plus redux-saga intro

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+
+import { fetchCollectionsStart } from "./shop/shop.sagas";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger); // it will only appear the logs on the development build
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
